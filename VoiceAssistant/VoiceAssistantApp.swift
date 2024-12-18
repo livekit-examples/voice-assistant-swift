30a9d6423125187676a651dd85b6c5329e1a764e
Ben Cherry <ben@livekit.io>
2024-12-17 16:16:24 -0800

    init

---
+++ VoiceAssistant/VoiceAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceAssistantApp.swift
+//  VoiceAssistant
+//
+//  Created by Ben Cherry on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
