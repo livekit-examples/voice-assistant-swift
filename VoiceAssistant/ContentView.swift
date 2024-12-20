fd8edb1536c2c1f23134a9f0ed9f62128e1e7e65
Ben Cherry <ben@livekit.io>
2024-12-20 12:22:26 -0800

    stuff

--- VoiceAssistant/ContentView.swift
+++ VoiceAssistant/ContentView.swift
@@ -4,61 +4,68 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isConnecting: Bool = false
-    @State private var connectionDetails: ConnectionDetails? = nil
+    @StateObject private var room = Room()
+
+    private var statusText: String {
+        switch room.connectionState {
+        case .disconnected:
+            "Disconnected"
+        case .connecting:
+            "Connecting"
+        case .reconnecting:
+            "Reconnecting"
+        case .connected:
+            "Connected"
+        }
+    }
 
     var body: some View {
         VStack(spacing: 24) {
-            if let connectionDetails {
-                RoomScope(
-                    url: connectionDetails.serverUrl,
-                    token: connectionDetails.participantToken,
-                    connect: true,
-                    enableCamera: false,
-                    enableMicrophone: true
-                ) {
-                    Text("Connected")
-                }
-            } else {
+            Text(statusText)
+            .multilineTextAlignment(.center)
+            .foregroundStyle(.secondary)
+            .padding(.horizontal)
 
-                Text(
-                    "Test"
-                )
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.secondary)
-                .padding(.horizontal)
-
-                ConnectButton(connectionDetails: $connectionDetails)
-            }
+            ConnectButton()
         }
         .padding()
+        .environmentObject(room)
     }
-    //    }
 }
 
 private struct ConnectButton: View {
     @EnvironmentObject private var tokenService: TokenService
+    @EnvironmentObject var room: Room
     @State private var isConnecting: Bool = false
-    @Binding var connectionDetails: ConnectionDetails?
 
     var body: some View {
         Button(action: {
-            isConnecting = true
             Task {
-                let roomName = "room-\(Int.random(in: 1000 ... 9999))"
-                let participantName = "user-\(Int.random(in: 1000 ... 9999))"
+                if room.connectionState != .disconnected {
+                    await room.disconnect()
+                } else {
+                    isConnecting = true
+                    let roomName = "room-\(Int.random(in: 1000 ... 9999))"
+                    let participantName = "user-\(Int.random(in: 1000 ... 9999))"
 
-                do {
-                    self.connectionDetails = try await tokenService.fetchConnectionDetails(
-                        roomName: roomName,
-                        participantName: participantName
-                    )
-                } catch {
-                    print("Connection error: \(error)")
-                    isConnecting = false
+                    do {
+                        if let connectionDetails = try await tokenService.fetchConnectionDetails(
+                            roomName: roomName,
+                            participantName: participantName
+                        ) {
+                            try await room.connect(url: connectionDetails.serverUrl, token: connectionDetails.participantToken)
+                        } else {
+                            print("Failed to fetch connection details")
+                        }
+                        isConnecting = false
+                    } catch {
+                        print("Connection error: \(error)")
+                        isConnecting = false
+                    }
                 }
             }
         }) {
-            Text(isConnecting ? "Connecting..." : "Start a Conversation")
+            Text(isConnecting ? "Connecting..." : room.connectionState == .connected ? "Disconnect" : "Start a Conversation")
         }
         .buttonStyle(.borderedProminent)
         .controlSize(.large)
