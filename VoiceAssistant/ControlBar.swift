abbd966d32af6276c363f6485fc1174469a79c0c
Ben Cherry <ben@livekit.io>
2024-12-20 14:44:17 -0800

    remove mic list

--- VoiceAssistant/ControlBar.swift
+++ VoiceAssistant/ControlBar.swift
@@ -6,56 +6,36 @@ struct ControlBar: View {
     @EnvironmentObject private var tokenService: TokenService
     @EnvironmentObject private var room: Room
     @State private var isConnecting: Bool = false
-    @State private var showingMicrophonePicker: Bool = false
 
     var body: some View {
         HStack(spacing: 16) {
             Spacer()
-
+            
             if room.connectionState == .connected {
-                HStack(spacing: 0) {
-                    HStack(spacing: 2) {
-                        Button(action: {
-                            Task {
-                                try? await room.localParticipant.setMicrophone(enabled: !room.localParticipant.isMicrophoneEnabled())
-                            }
-                        }) {
-                            Label {
-                                Text(room.localParticipant.isMicrophoneEnabled() ? "Mute" : "Unmute")
-                            } icon: {
-                                Image(systemName: room.localParticipant.isMicrophoneEnabled() ? "mic" : "mic.slash")
-                            }
-                            .labelStyle(.iconOnly)
-                        }
-                        .buttonStyle(.plain)
-                        .frame(width: 44, height: 44)
-
-                        LocalAudioVisualizer(track: room.localParticipant.firstAudioTrack)
-                            .frame(height: 44)
-                            .id(room.localParticipant.firstAudioTrack?.id ?? "no-track") // ensure the component re-renders if the track changes
-                    }
-                    .background(.primary.opacity(0.1))
-
+                HStack(spacing: 2) {
                     Button(action: {
-                        showingMicrophonePicker.toggle()
+                        Task {
+                            try? await room.localParticipant.setMicrophone(enabled: !room.localParticipant.isMicrophoneEnabled())
+                        }
                     }) {
-                        Image(systemName: "chevron.down")
+                        Label {
+                            Text(room.localParticipant.isMicrophoneEnabled() ? "Mute" : "Unmute")
+                        } icon: {
+                            Image(systemName: room.localParticipant.isMicrophoneEnabled() ? "mic" : "mic.slash")
+                        }
+                        .labelStyle(.iconOnly)
                     }
                     .buttonStyle(.plain)
                     .frame(width: 44, height: 44)
-                    .popover(isPresented: $showingMicrophonePicker) {
-                        VStack(alignment: .leading) {
-                            Text("Select Microphone")
-                                .font(.headline)
-                                .padding()
-                            // Add your microphone list here
-                        }
-                        .frame(minWidth: 300)
-                    }
-                }.background(.primary.opacity(0.1))
-                    .cornerRadius(8)
+                    
+                    LocalAudioVisualizer(track: room.localParticipant.firstAudioTrack)
+                        .frame(height: 44)
+                        .id(room.localParticipant.firstAudioTrack?.id ?? "no-track") // ensure the component re-renders if the track changes
+                }
+                .background(.primary.opacity(0.1))
+                .cornerRadius(8)
             }
-
+            
             Button(action: {
                 Task {
                     if room.connectionState != .disconnected {
@@ -64,7 +44,7 @@ struct ControlBar: View {
                         isConnecting = true
                         let roomName = "room-\(Int.random(in: 1000 ... 9999))"
                         let participantName = "user-\(Int.random(in: 1000 ... 9999))"
-
+                        
                         do {
                             if let connectionDetails = try await tokenService.fetchConnectionDetails(
                                 roomName: roomName,
@@ -101,7 +81,7 @@ struct ControlBar: View {
             .buttonStyle(.borderedProminent)
             .controlSize(.large)
             .disabled(isConnecting)
-
+            
             Spacer()
         }
     }
