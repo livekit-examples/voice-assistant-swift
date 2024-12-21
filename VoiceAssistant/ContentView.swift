52e07394e9094d30dd03b87eb7246d5e44b7090d
Ben Cherry <ben@livekit.io>
2024-12-20 17:19:29 -0800

    Voice Assistant Example App (#1)
    * add token service
    
    * improved token service
    
    * connect
    
    * ref
    
    * stuff
    
    * agent state
    
    * bar
    
    * updates
    
    * Local viz
    
    * state
    
    * remove mic list
    
    * better
    
    * better
    
    * krisp
    
    * readme
    
    * co
    
    * vision
    
    * selector
    
    * Comments etc
    
    * md

--- VoiceAssistant/ContentView.swift
+++ VoiceAssistant/ContentView.swift
@@ -1,24 +1,38 @@
-//
-//  ContentView.swift
-//  VoiceAssistant
-//
-//  Created by Ben Cherry on 12/17/24.
-//
-
+import LiveKit
 import SwiftUI
+#if os(iOS) || os(macOS)
+import LiveKitKrispNoiseFilter
+#endif
 
 struct ContentView: View {
+    @StateObject private var room = Room()
+
+// Krisp is available only on iOS and macOS right now
+// Krisp is also a feature of LiveKit Cloud, so if you're using open-source / self-hosted you should remove this
+#if os(iOS) || os(macOS)
+    private let krispProcessor = LiveKitKrispNoiseFilter()
+#endif
+    
+    init() {
+#if os(iOS) || os(macOS)
+        AudioManager.shared.capturePostProcessingDelegate = krispProcessor
+#endif
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 24) {
+            StatusView()
+                .frame(height: 256)
+                .frame(maxWidth: 512)
+            
+            ControlBar()
         }
         .padding()
+        .environmentObject(room)
+        .onAppear {
+#if os(iOS) || os(macOS)
+            room.add(delegate: krispProcessor)
+#endif
+        }
     }
-}
-
-#Preview {
-    ContentView()
 }
--- VoiceAssistant/VoiceAssistantApp.swift
+++ VoiceAssistant/VoiceAssistantApp.swift
@@ -1,17 +1,13 @@
-//
-//  VoiceAssistantApp.swift
-//  VoiceAssistant
-//
-//  Created by Ben Cherry on 12/17/24.
-//
-
 import SwiftUI
 
 @main
 struct VoiceAssistantApp: App {
+    private var tokenService: TokenService = .init()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(tokenService)
         }
     }
 }
