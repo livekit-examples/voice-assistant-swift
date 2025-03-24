b967c4814c4f3d82f05f50e3255635ffc8cda0d7
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-03-24 11:39:27 +0100

    Extension

--- VoiceAssistant/StatusView.swift
+++ VoiceAssistant/StatusView.swift
@@ -8,27 +8,9 @@ struct StatusView: View {
     // Load the room from the environment
     @EnvironmentObject private var room: Room
 
-    // Find the first agent participant in the room
-    // This assumes there's only one agent and they publish only one audio track
-    // Your application may have more complex requirements
-    private var agentParticipant: RemoteParticipant? {
-        for participant in room.remoteParticipants.values {
-            if participant.kind == .agent {
-                return participant
-            }
-        }
-
-        return nil
-    }
-
-    // Reads the agent state property which is updated automatically
-    private var agentState: AgentState {
-        agentParticipant?.agentState ?? .initializing
-    }
-
     var body: some View {
-        if let participant = agentParticipant {
-            AgentBarAudioVisualizer(audioTrack: participant.firstAudioTrack, agentState: agentState, barColor: .primary, barCount: 9)
+        if let participant = room.agentParticipant {
+            AgentBarAudioVisualizer(audioTrack: participant.firstAudioTrack, agentState: room.agentState, barColor: .primary, barCount: 5)
                 .id(participant.firstAudioTrack?.id)
         }
     }
