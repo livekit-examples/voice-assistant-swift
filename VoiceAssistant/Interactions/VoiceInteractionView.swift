d0780ac606787f2550c72850c1b59314411ec790
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-05-28 13:06:59 +0200

    iPad/mac layout

--- VoiceAssistant/Interactions/VoiceInteractionView.swift
+++ VoiceAssistant/Interactions/VoiceInteractionView.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct VoiceInteractionView: View {
     @Environment(AppViewModel.self) private var viewModel
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     var namespace: Namespace.ID
 
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            AgentParticipantView(namespace: namespace)
-            HStack {
-                LocalParticipantView(namespace: namespace)
-                    .frame(maxWidth: 120, maxHeight: 200)
-                ScreenShareView(namespace: namespace)
-                    .frame(maxWidth: 200, maxHeight: 200)
+        Group {
+            if horizontalSizeClass == .regular {
+                ZStack(alignment: .bottomTrailing) {
+                    AgentParticipantView(namespace: namespace)
+                        .frame(maxWidth: .infinity)
+                    VStack {
+                        ScreenShareView(namespace: namespace)
+                            .frame(maxWidth: 200, maxHeight: 200)
+                        LocalParticipantView(namespace: namespace)
+                            .frame(maxWidth: 200, maxHeight: 200)
+                    }
+                }
+            } else {
+                ZStack(alignment: .topTrailing) {
+                    AgentParticipantView(namespace: namespace)
+                    HStack {
+                        LocalParticipantView(namespace: namespace)
+                            .frame(maxWidth: 120, maxHeight: 200)
+                        ScreenShareView(namespace: namespace)
+                            .frame(maxWidth: 200, maxHeight: 200)
+                    }
+                }
             }
         }
         .padding(.horizontal)
